68d66c5924691f9255426ae1a5d1bdea57af3371
yuemintang <shirleytang1996@gmail.com>
2019-03-11 02:51:49 -0400

    commit 4
    set up reactDOM .render

---
+++ src/app.js
@@ -0,0 +1,9 @@
+class AddressBook extends React.Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+}
+
+ReactDOM.render(<AddressBook/>,document.getElementById("app"))
